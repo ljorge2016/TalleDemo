6d52199a3e003da45cf55b4f6db4cf3b612f7000
ljorge2016 <jluis.emrique@gmail.com>
2021-05-04 15:21:47 -0500

    Creaciòn de la entidad Producto

---
+++ BOPE/Entidades/Producto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BOPE.Entidades
+{
+    public class Producto
+    {
+        public int ID { get; set; }
+        public decimal Precio { get; set; }
+        public string nombre { get; set; }
+
+    }
+}
